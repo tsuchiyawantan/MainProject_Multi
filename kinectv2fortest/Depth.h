464c844e335547830a428a6c8ffa791462be5dce
Momoko Tsuchiya <tsuchiya.momoko@is.ocha.ac.jp>
2016-06-29 14:05:08 +0900

    person by person succeed

--- kinectv2fortest/Depth.h
+++ kinectv2fortest/Depth.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <iostream>
 #include <sstream>
-#include <thread>
-#include <mutex>
 #include <Kinect.h>
 #include <opencv2/opencv.hpp>
 #include <atlbase.h>
@@ -16,42 +14,77 @@ using namespace std;
 #define HIGHT 200
 #define WIDTH 200
 
+class Human{
+private:
+	int humanID;
+	int depthMax;
+	int depthMin;
+public:
+
+
+	Human(){
+		init();
+	}
+	~Human(){}
+
+	void setHumanID(int i){
+		humanID = i;
+	}
+	void setDepthMax(int i){
+		depthMax = i;
+	}
+	void setDepthMin(int i){
+		depthMin = i;
+	}
+	void init(){
+		setHumanID(-1);
+		setDepthMax(-1);
+		setDepthMin(INFINITE);
+	}
+	int getHumanID(){
+		return this->humanID;
+	}
+	int getDepthMax(){
+		return this->depthMax;
+	}
+	int getDepthMin(){
+		return this->depthMin;
+	}
+};
+
 class Depth : public KinectControl
 
 {
 	// ******** depth *******
 private:
 public:
-	mutex mtx;
-	int depthMax;
-	int depthMin;
 	cv::Mat bodyDepthImage;
 	cv::Mat normalizeDepthImage;
 	cv::Mat contourImage;
-
-	//êlêîÇï‘Ç∑
-	int findHuman(){		
-		updateBodyIndexFrame();
-
-	}
-	void findDepthMaxMin(int y, int x, int a){
-		if (depthMax < a) depthMax = a;
-		if (depthMin > a) depthMin = a;
+	Human human[6];
+	
+	void findDepthMaxMin(Human &human, int depth){
+		if (human.getDepthMax() < depth) human.setDepthMax(depth);
+		if (human.getDepthMin() > depth) human.setDepthMin(depth);
 	}
 	void setBodyDepth(){
 		updateDepthFrame();
+		updateBodyIndexFrame();
 		bodyDepthImage = cv::Mat(depthHeight, depthWidth, CV_16UC1);
-		depthMax = -1;
-		depthMin = INFINITE;
+		for (int i = 0; i < 6; i++) human[i].init();
 		for (int i = 0; i < bodyIndexHeight*bodyIndexWidth; i++) {
 			int y = i / bodyIndexWidth;
 			int x = i % bodyIndexWidth;
-			if (bodyIndexBuffer[i] == 255) {
+			int id = bodyIndexBuffer[i];
+			if (id == 255) {
 				bodyDepthImage.at<UINT16>(y, x) = 65535;
+			}
+		else if (human[id].getHumanID() == -1){
+				human[id].setHumanID(id);
 			}
 			else {
 				bodyDepthImage.at<UINT16>(y, x) = depthBuffer[i];
-				findDepthMaxMin(y, x, depthBuffer[i]);
+				findDepthMaxMin(human[id], depthBuffer[i]);
 			}
 		}
 	}
@@ -60,9 +93,10 @@ public:
 		for (int i = 0; i < srcImg.rows*srcImg.cols; i++) {
 			int y = i / srcImg.cols;
 			int x = i % srcImg.cols;
-			if (bodyIndexBuffer[i] == 255) normalizeDepthImage.at<UCHAR>(y, x) = 255;
+			int id = bodyIndexBuffer[i];
+			if (id == 255) normalizeDepthImage.at<UCHAR>(y, x) = 255;
 			else {
-				normalizeDepthImage.at<UCHAR>(y, x) = (255 * (srcImg.at<UINT16>(y, x) - depthMin)) / (depthMax - depthMin);
+				normalizeDepthImage.at<UCHAR>(y, x) = (255 * (srcImg.at<UINT16>(y, x) - human[id].getDepthMin())) / (human[id].getDepthMax() - human[id].getDepthMin());
 			}
 		}
 	}
